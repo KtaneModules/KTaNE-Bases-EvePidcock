12fc18bcfc3159bbe85bdd1c97f99a35c8738d41
AAces <djp0908@yahoo.com>
2018-10-03 08:25:14 -0600

    Initial creation

---
+++ Assets/bases.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class bases : MonoBehaviour {
+    private static int _moduleIdCounter = 1;
+    private bool _isSolved, _lightsOn;
+    private int _moduleId;
+    public KMAudio newAudio;
+    public KMBombInfo info;
+    public KMBombModule module;
+
+    private void Start()
+    {
+        _moduleId = _moduleIdCounter++;
+        module.OnActivate += Activate;
+    }
+
+    private void Activate()
+    {
+        Init();
+        _lightsOn = true;
+    }
+
+    private void Init() {
+
+    }
+
+    public string IntToString(int a, int radix)
+    {
+        var chars = "0123456789ABCDEF".ToCharArray();
+        var str = new char[32]; // maximum number of chars in any base
+        var i = str.Length;
+        bool isNegative = (a < 0);
+        if (a <= 0) // handles 0 and int.MinValue special cases
+        {
+            str[--i] = chars[-(a % radix)];
+            a = -(a / radix);
+        }
+
+        while (a != 0)
+        {
+            str[--i] = chars[a % radix];
+            a /= radix;
+        }
+
+        if (isNegative)
+        {
+            str[--i] = '-';
+        }
+
+        return new string(str, i, str.Length - i);
+    }
+}
